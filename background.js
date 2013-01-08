39a6a289b568eb9e7af663356b7119d735d72b5b
Ed Shelley <edshelley@gmail.com>
2013-01-08 08:45:08 +0000

    added onload.js (Google) to make oauth redirect work

--- background.js
+++ background.js
@@ -6,15 +6,16 @@ var oauth = ChromeExOAuth.initBackgroundPage({
   'request_url': 'https://trello.com/1/OAuthGetRequestToken',
   'authorize_url': 'https://trello.com/1/OAuthAuthorizeToken',
   'access_url': 'https://trello.com/1/OAuthGetAccessToken',
-  'consumer_key': 'appKey',
-  'consumer_secret': 'appSecret',
-  'scope': '',//<scope of data access, not used by all OAuth providers>,
+  'consumer_key': appKey,
+  'consumer_secret': appSecret,
+  //'scope': '',//<scope of data access, not used by all OAuth providers>,
   'app_name': 'Shello'
 });
 
 
 oauth.authorize(function() {
   // ... Ready to fetch private data ...
+  var test = 'blah';
 });
 
 function callback(resp, xhr) {
