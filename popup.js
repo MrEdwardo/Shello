ae022d8504c5b706048d2cff06947c7d020648a6
Ed Shelley <edshelley@gmail.com>
2013-01-09 23:39:58 +0000

    First functional version. Finds the story card and outputs to popup

--- popup.js
+++ popup.js
@@ -3,63 +3,115 @@
 // to the currently open card on Trello.com
 
 var oauth = chrome.extension.getBackgroundPage().oauth;
+//var storyId = null;
 oauth.authorize(onAuthorized);
 
-chrome.tabs.getSelected(null,function(tab) {
-    //var cardId = getIdFromCard(tab.url);
-    getStoryIdFromCard(tab.url);
-});
-
 function onAuthorized() {
+  chrome.tabs.getSelected(null,function(tab) {
+    getCurrentCardObject(tab.url);
+  });
+};
+
+function getCurrentCardObject(url) {
+  //var storyId = url.match(/\[(\d+\.?\d*)\]/);
+  //extract card ID from the url
+  var cardId = getCardIdFromUrl(url);
+  var boardGuid = getBoardIdFromUrl(url);
+  var requestUrl = 'https://api.trello.com/1/boards/' + boardGuid + '/cards/' + cardId;
+  var request = {
+    'method': 'GET',
+  };
+  //get the card object:
+  oauth.sendSignedRequest(requestUrl, function(resp, xhr){getCard_Callback(resp, xhr, boardGuid)}, request);
+}
+
+function getCard_Callback(resp, xhr, boardGuid) {
+  var card = jQuery.parseJSON(resp);
+  var name = card.name;
+  var regex = name.match(/\[(s\d+s)\]/);
+  var storyId = null;
+
+  if(regex[1]!= null){
+    storyId = regex[1];//.replace(/s/gi, '');
+  }
+
+  // get current board name:
+  var currentBoardUrl = 'https://trello.com/1/boards/' + boardGuid;
+  var request = {
+    'method': 'GET',
+  };
+  oauth.sendSignedRequest(currentBoardUrl, function(resp, xhr){getCurrentBoard_Callback(resp, xhr, storyId) }, request);
+}
+
+function getCurrentBoard_Callback(resp, xhr, storyId){
+  var board = jQuery.parseJSON(resp);
+
+  // Now look at user's boards for a match.
   var url = 'https://trello.com/1/members/my/boards';
   var request = {
     'method': 'GET',
   };
-  oauth.sendSignedRequest(url, callback, request);
-};
+  oauth.sendSignedRequest(url, function(resp, xhr) {
+    var boards = jQuery.parseJSON(resp);
+    findMatchingStoriesBoard(board, boards, storyId);
+  }, request);
 
-function callback(resp, xhr) {
-  var collection = jQuery.parseJSON(resp);
-  var boards = [];
+}
 
-  for(var i in collection) {
-    //console.log(collection[i]); DEBUG
-    if(collection[i].hasOwnProperty('name')) {
-      var value = collection[i].name;
-      if(value.toLowerCase().indexOf('stories') != -1) {
-        //board name contains 'stories'
-        boards.push(collection[i]); //add to array
-      }
+function findMatchingStoriesBoard(currentBoard, potentialBoards, storyId) {
+  var trimmed = currentBoard.name.toLowerCase().replace(' tasks', '');
+  for(var board in potentialBoards){
+    var trimmed2 = potentialBoards[board].name.toLowerCase().replace(' stories', '');
+    if(trimmed2.indexOf(trimmed) != -1){
+      // FOUND IT!
+      var currentBoardUrl = 'https://trello.com/1/boards/' + potentialBoards[board].id + '/cards/open';
+      var request = {
+        'method': 'GET',
+      };
+      oauth.sendSignedRequest(currentBoardUrl, function(resp, xhr){
+        var cards = jQuery.parseJSON(resp);
+        findMatchingStoryCard(cards, storyId);
+      }, request);
     }
   }
-};
+  return null;
+}
 
-function getIdFromCard(url) {
+function findMatchingStoryCard(cards, storyId){
+
+  for(var card in cards){
+    var name = cards[card].name;
+    if(name.indexOf(storyId) != -1){
+      // this is our story card!!
+      console.log("FOUND THE BLOODY CARD: " + cards[card].name);
+      var text = document.createTextNode(cards[card].name);
+      document.body.appendChild(text);
+    }
+  }
+}
+
+// URL BASED FUNCTIONS__________________
+function getCardIdFromUrl(url) {
   if(url.indexOf('https://trello.com') == -1){
     return -1;
   }
   var split = url.split("/");
-  var cardId = split[split.length - 2];
+  var cardId = split[split.length - 1];
 
-    if(cardId.length > 10) { // check for a reasonable length ID
+    if(cardId.length < 5) { // check for a reasonable length ID
       return cardId;
     }
   return -1;
 }
 
-function getStoryIdFromCard(url) {
-  var storyId = url.match(/\[(\d+\.?\d*)\]/);
-  //extract card ID from the url
-  cardGuid = getIdFromCard(url);
-  var requestUrl = 'https://trello.com/1/cards/' + cardGuid + '?fields=name';
-  var request = {
-    'method': 'GET',
-  };
-  //get the card object:
-  oauth.sendSignedRequest(requestUrl, callback2, request);
-}
-
-function callback2(resp, xhr) {
-  var card = jQuery.parseJSON(resp);
-  var name = card.name;
+function getBoardIdFromUrl(url) {
+  if(url.indexOf('https://trello.com') == -1){
+    return -1;
+  }
+  var split = url.split("/");
+  var boardId = split[split.length - 2];
+  if(boardId.length > 10) {
+    return boardId;
+  }
+  return -1;
 }
